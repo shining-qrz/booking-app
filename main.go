8855691f227bdff65a8da16ee259760f12704dab
shining <1909906595@qq.com>
2025-10-05 16:06:01 +0800

    feat: 使用":="语法糖

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference" //等价于 var conferenceName string = "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	remainingTickets := 50
 	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
