4c896f47cba0f756089a0ad595f0d20c44ae8c4d
shining <1909906595@qq.com>
2025-10-05 22:40:28 +0800

    feat: 添加基本会议票扣减逻辑

--- main.go
+++ main.go
@@ -5,8 +5,27 @@ import "fmt"
 func main() {
 	conferenceName := "Go Conference" //等价于 var conferenceName string = "Go Conference"
 	const conferenceTickets = 50
-	remainingTickets := 50
+	var remainingTickets uint = 50
+
+	var username string
+	var ticketCount uint
+	var email string
+
 	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
+
+	fmt.Println("请输入用户名：")
+	fmt.Scan(&username)
+
+	fmt.Println("请输入需预定票的数量：")
+	fmt.Scan(&ticketCount)
+
+	fmt.Println("请输入您的邮箱：")
+	fmt.Scan(&email)
+
+	remainingTickets = remainingTickets - ticketCount
+
+	fmt.Printf("恭喜，%v！您已成功预定%v张票，我们稍后将会发送邮件至：%v\n", username, ticketCount, email)
+
+	fmt.Println("剩余票数：", remainingTickets)
 }
