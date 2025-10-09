b64e9f7e6f66262c8f75bcaa75c55743b6d33a38
shining <1909906595@qq.com>
2025-10-09 20:01:07 +0800

    feat: Array And Slice

--- main.go
+++ main.go
@@ -11,6 +11,14 @@ func main() {
 	var ticketCount uint
 	var email string
 
+	// 定义数组并初始化
+	// var bookings = [50]string {"张三", "李四", "王五"}
+
+	// 语法糖：定义空数组
+	// var bookings [50]string
+
+	var bookings []string
+
 	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 
@@ -25,7 +33,15 @@ func main() {
 
 	remainingTickets = remainingTickets - ticketCount
 
+	bookings = append(bookings, username)
+
 	fmt.Printf("恭喜，%v！您已成功预定%v张票，我们稍后将会发送邮件至：%v\n", username, ticketCount, email)
 
 	fmt.Println("剩余票数：", remainingTickets)
+
+	fmt.Printf("The Whole Slice: %v\n", bookings)
+	fmt.Printf("The First Value: %v\n", bookings[0])
+	fmt.Printf("The Lenth of Slice: %v\n", len(bookings))
+	fmt.Printf("The Type of Slice: %T\n", bookings)
+
 }
